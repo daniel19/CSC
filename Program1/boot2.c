5ba7b74afaa4b602de2c7172c658673b871b3b77
Coursework <coursework@CSC3550.csc.tntech.edu>
2014-02-04 08:35:00 -0600

    added comments

---
+++ Program1/boot2.c
@@ -0,0 +1,24 @@
+//boot2.c is the c driver for our first project
+//Created 2.3.2014 by Daniel Brown 
+//CSC 4100 - Operating Systems for Professor Mike Rogers
+#include <stdio.h>
+
+//Forward declartions
+void clearScr();
+void writeScr( char *string, int row, int col);
+
+int main(){
+    //Main function that calls clearScr() and writeScr() functions 
+    clearScr();
+    writeSrc("Hello  World", 12, 25);//Prints string onto screen after booting up
+    while(1);
+    return 0;
+}
+
+void clearScr(){
+}
+
+void writeScr(char *string, int row, int col){
+    //calcualte offset into video memory
+}
+
