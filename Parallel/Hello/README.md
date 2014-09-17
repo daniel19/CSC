44841142df79143f4611b42b9b281aa144229c93
dbbrown42 <dbbrown42@shell.csc.tntech.edu>
2014-09-17 09:03:16 -0500

    Addign Program1

---
+++ Parallel/Hello/README.md
@@ -0,0 +1,5 @@
+To run and compile omp threads use the following commands:
+    gcc/g++ -g -Wall -fopenmp 
+
+To run and compile pthread threads use the following commands:
+    gcc/g++ -g -Wall -lpthread 
---
+++ Parallel/Program1/matrix.cpp
@@ -0,0 +1,24 @@
+/**
+ *matrix.cpp
+ *Pthread application that computes matrix multiplication using
+ *  threading applications.
+ *@author Daniel Brown
+ *@version 1.0 9/16/2014
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <pthread.h>
+
+
+/*
+ * Measure the time for the serial portion.
+ *  *Variable declaration and thread declaration
+ * Then measure running threads.
+*/
+int main(){
+   printf("Hello World");
+   return 0;
+}
