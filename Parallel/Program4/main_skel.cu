664be64242640260c1684ac4436904988c264849
Daniel Brown <daniel19tiny>
2014-11-29 12:28:36 -0600

    No errors found at compile time. Expected Result not found.

--- Parallel/Program4/main_skel.cu
+++ Parallel/Program4/main_skel.cu
@@ -8,11 +8,37 @@
  *Kernel will update the matrix to keep the heater cells constant.
  */
 __global__ void copyHeaters(float* stateGrid, float* heaterGrid, int nRows, int nCols) {
-   //TODO 
+    for(int i = 0; i < nRows; i++){
+        for(int j = 0; j < nCols; j++){
+            float heatValue = heaterGrid[i*nCols +j];
+            if(heatValue != 0)
+                stateGrid[i*nCols + j] =heatValue;
+        }
+    }
 }
 
 __global__ void updateGrid(float* inGrid, float* outGrid, float k, int nRows, int nCols) {
-   //TODO 
+    //Find these values from the inGrid
+    int Tlft, Trite, Tup, Tdown;
+    for(int i=0; i < nRows; ++i){
+       for(int j=0; j < nCols; ++j){
+           int currentPosition = i*nCols+j;
+           Tlft = currentPosition +1;
+           Trite = currentPosition -1;
+           Tup = currentPosition +4;
+           Tdown = currentPosition -4;
+           float Tnew = inGrid[currentPosition];
+           float Top, Tbottom, Tleft, Tright;
+           Tbottom = (Tdown < 0) ? 0 : inGrid[Tdown];
+           Top = (Tup > nCols) ? 0 : inGrid[Tup];
+           Tright = (Trite < 0) ?  0 : inGrid[Trite];
+           Tleft = (Tlft > nCols) ? 0 : inGrid[Tlft]; 
+           
+            Tnew = Tnew + k*(Top + Tbottom + Tleft + Tright - (4*Tnew));
+
+           outGrid[currentPosition] = Tnew;
+       }
+   } 
 }
 
 /*------------------------------------------------------------------------------
@@ -96,28 +122,36 @@ int main(int argc, char** argv) {
 
     float* heaterGrid_d; //device pointer
     //TODO Copy heater grid to device
+    cudaMalloc(&heaterGrid_d, gridSize);
+    cudaMemcpy(heaterGrid_d, heaterGrid_h, gridSize, cudaMemcpyDeviceToHost);
+
 
     //Input grid
     float* inGrid_h = (float*)malloc(gridSize);
     memset(inGrid_h, 0, gridSize);
     float* inGrid_d; //device pointer
     //TODO Allocate and copy inGrid to device
+    cudaMalloc(&inGrid_d, gridSize);
+    cudaMemcpy(inGrid_d, inGrid_h, gridSize, cudaMemcpyDeviceToHost);
 
     //Output grid
     float* outGrid_h = (float*)malloc(gridSize);
     memset(outGrid_h, 0, gridSize);
     float* outGrid_d; //device pointer
     //TODO Allocate and copy outGrid to device
+    cudaMalloc(&outGrid_d, gridSize);
+    cudaMemcpy(outGrid_d, outGrid_h, gridSize, cudaMemcpyDeviceToHost);
 
     //TODO fill in update loop
     for(int i = 0; i < timeSteps; ++i) {
         //copy heater temps to inGrid_d (kernel call)
+        copyHeaters<<<1, 1>>>(inGrid_d, heaterGrid_d, rows, cols);
         //update outGrid_d based on inGrid_d (kernel call)
         //swap pointers inGrid_d and outGrid_d
     }
     
     //TODO copy inGrid_d back to host (to inGrid_h)
-    
+    cudaMemcpy(inGrid_h, inGrid_d, gridSize, cudaMemcpyHostToDevice); 
     printGridToFile(inGrid_h, rows, cols, "output.txt");
 
     return 0;
